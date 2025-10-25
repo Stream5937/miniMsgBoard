0f311420f525001e91bebf61950b04a49120f5f6
Stream5937 <149094480+Stream5937@users.noreply.github.com>
2025-10-25 17:22:46 +0100

    relocate new message form to sidebar

--- routes/aboutRouter.js
+++ routes/aboutRouter.js
@@ -11,8 +11,11 @@ const links = [
 
 // When this router is mounted at '/about' in app.js, use the root path here
 aboutRouter.get("/", (req, res) => {
+  // read ?create=true from query string and convert to boolean
+  const create =
+    typeof req.query.create !== "undefined" && req.query.create === "true";
   // render the 'about' view (no leading slash)
-  res.render("about", { title: "About", links: links });
+  res.render("about", { title: "About", links: links, create: create });
 });
 
 module.exports = aboutRouter;
